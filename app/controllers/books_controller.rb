b4204cea92fe8590adef321fc475a32d177a7f16
Obtuse Autodidact <theobtuseautodidact@gmail.com>
2015-03-30 18:07:10 -0600

    added book user association

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :edit, :create, :update, :destroy]
   before_action :set_book, only: [:show, :edit, :update, :destroy]
 
   # GET /books
@@ -25,7 +26,9 @@ class BooksController < ApplicationController
   # POST /books
   # POST /books.json
   def create
-    @book = Book.new(book_params)
+    @book = current_user.books.new(book_params)
+    #@book.save
+    #respond_with(@book)   In tiago's file not auto-generated in mine
 
     respond_to do |format|
       if @book.save
